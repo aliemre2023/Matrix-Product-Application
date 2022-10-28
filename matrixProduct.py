c895f8cb46f626b978049af9d59538349770f9cc
kayaemr21 <aliemrekaya239@gmail.com>
2022-10-28 20:20:38 +0300

    excess codes deleted.

--- matrixProduct.py
+++ matrixProduct.py
@@ -13,8 +13,8 @@ Please enter only 9 numbers or application will not work.
 
 """)
 
-matrix1 = input("Input 9 number for matrix 1 (seperate with comma[,]): ").split(",")
-matrix2 = input("Input 9 number for matrix 2 (seperate with comma[,]): ").split(",")
+matrix1 = input("Input 9 number for matrix 1 (separate with comma[,]): ").split(",")
+matrix2 = input("Input 9 number for matrix 2 (separate with comma[,]): ").split(",")
 
 n1 = np.array(matrix1)
 n2 = np.array(matrix2)
@@ -29,57 +29,15 @@ answer = list()
 for i in range(3):
     for j in range(3):
         for x in range(3):
-            pro = m1[i][x] * m2[x][j]
-            answer.append(pro)
-
-donus = 0
-tot11 = 0
-tot12 = 0
-tot13 = 0
-tot21 = 0
-tot22 = 0
-tot23 = 0
-tot31 = 0
-tot32 = 0
-tot33 = 0
-while donus < 3:
-    tot11 += answer[donus]
-    donus += 1
-while donus < 6:
-    tot12 += answer[donus]
-    donus += 1
-while donus < 9:
-    tot13 += answer[donus]
-    donus += 1
-while donus < 12:
-    tot21 += answer[donus]
-    donus += 1
-while donus < 15:
-    tot22 += answer[donus]
-    donus += 1
-while donus < 18:
-    tot23 += answer[donus]
-    donus += 1
-while donus < 21:
-    tot31 += answer[donus]
-    donus += 1
-while donus < 24:
-    tot32 += answer[donus]
-    donus += 1
-while donus < 27:
-    tot33 += answer[donus]
-    donus += 1
+            pri = m1[i][x] * m2[x][j]
+            answer.append(pri)
 
 realAnswer = list()
-realAnswer.append(tot11)
-realAnswer.append(tot12)
-realAnswer.append(tot13)
-realAnswer.append(tot21)
-realAnswer.append(tot22)
-realAnswer.append(tot23)
-realAnswer.append(tot31)
-realAnswer.append(tot32)
-realAnswer.append(tot33)
+o = 0
+for i in range(9):
+    total = answer[o]+answer[o+1]+answer[o+2]
+    o += 3
+    realAnswer.append(total)
 
 final = np.array(realAnswer).reshape(3,3)
 
